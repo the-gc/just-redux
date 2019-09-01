7bca04de5641356735ca36e7029f23971615a935
gucheng <gcloudy@qq.com>
2019-09-01 23:53:58 +0800

    react-redux 基础实例

---
+++ my-app/src/containers/FilterLink.js
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux'
+import { setVisibilityFilter } from '../actions'
+import Link from '../components/Link'
+
+
+const mapStateToProps = (state, ownProp) => {
+    return {
+        active: ownProp.filter == state.visibilityFilter
+    }
+} 
+
+const mapDispatchToProps = (dispatch, ownProp) => {
+    return {
+        onClick: () => {
+            dispatch(setVisibilityFilter(ownProp.filter))
+        }
+    }
+}
+
+const FilterLink = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Link)
+
+export default FilterLink
--- my-app/src/reducers/todos.js
+++ my-app/src/reducers/todos.js
@@ -7,10 +7,10 @@ const todos = (state = [], action) => {
                 ...state, {id: action.id, text: action.text, completed: false}
             ]
         case 'TOGGLE_TODO':
-            return state.map(todo => {
+            return state.map(todo => (
                 (todo.id === action.id)
                   ? {...todo, completed: !todo.completed} : todo
-            })
+            ))
         default:
             return false
     }
